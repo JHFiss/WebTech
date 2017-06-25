e966443c0997b10615bc65046e2b6e65b0b08fec
JULI-LAPTOP\julip <juliparr94@gmail.com>
2017-06-25 15:51:28 +0200

    Controller reagiert auf Touch. Der Tick läuft im Controller. Zum setzten eines neuen Touch Befehls muss momentan der Finger kurz vom bildschirm genommen werden und an der gewünschten position neu plaziert werden.

--- web/controller.dart
+++ web/controller.dart
@@ -13,14 +13,23 @@ class Controller {
   Controller(View this._view);
   String event = "";
   String ret = "";
+  int _maxCol = 15;
+  int _maxRow = 15;
   void start() {
-    Element target2;
-    querySelectorAll('#gameTable td').onMouseOver.forEach((ev) {
+    Element target;
+    //Sorgt dafür das alle tabellen elemente auf Maus hover reagieren
+    querySelectorAll('#gameTable td').onTouchStart.listen((ev) {
       ev.preventDefault();
-      target2 = ev.target;
-      querySelector('#start').text = 'row =${target2.getAttribute('row')}'
-          ' || col =${target2.getAttribute('col')}';
-          });
+      target = ev.target;
+    });
+      //Aktualisiert alle .. milisekunden das spiel
+      Timer timer = new Timer.periodic(new Duration(milliseconds: 1000), (t) {
+        if(target != null) {
+          querySelector('#start').text = 'row =${target.getAttribute('row')}'
+              ' || col =${target.getAttribute('col')}';
+        }
+      });
+
     this._view.updateView(ret);
   }
 }
