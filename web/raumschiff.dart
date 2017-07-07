d72801916587b3ba08ebe2537a95fe74b8b1592e
Jan H. Fiss <jhfiss@gmail.com>
2017-07-07 19:51:43 +0200

    XSD ueberarbeitet, sequence-Fehler immernoch vorhanden, Clutter aus dem Raumschiff, der Koerperform entfernt @Julian: Benutz die verdammten Schnittstellen!

--- web/raumschiff.dart
+++ web/raumschiff.dart
@@ -17,48 +17,11 @@ class Raumschiff extends Koerperform {
   ///Returned die Laser ID referenze
   int get getLaserIDref => this._laserIDref;
 
-  //TODO Implementieren
-  void _setPosition(Koordinaten koordinaten) {
-    int xOffset,yOffset = 0;
-    int xNextMove = 0;  //1=Rechts, 0=stehenbleiben, -1 Links
-    int yNextMove = 0;  //1=Oben, 0=stehenbleiben, -1Links
-    Koordinaten neueKoor;
-
-    xOffset = koordinaten.getX - getPos.getX;
-    yOffset = koordinaten.getY - getPos.getY;
-
-    if(xOffset != 0) {
-      xNextMove = xOffset > 0 ? 1 : -1;
-    }
-    if(yOffset != 0) {
-      yNextMove = yOffset > 0 ? 1 : -1;
-    }
-
-    //Erstellt die neue position der koeperform
-    //TODO wieder einfügern
-    //neueKoor = new Koordinaten((getPos.getX + xNextMove), (getPos.getY + yNextMove));
-
-    neueKoor = new Koordinaten(5, 5);
-    setPosition(neueKoor);  //Ruft das setPosition aus der Koeperform auf
-  }
-
   //TODO zzt Pseudocode, für korrekte Implementierung muessen collisionDetection und setPosition funktionieren
   //TODO Spielergesteuertes Raumschiff, Steuervariablen müssen uebergeben werden
   @override
   void onTick(Koordinaten koordinaten) {
-    _setPosition(koordinaten);
-    positonToSpielfeld();
+
   }
 
-  /*void _positonToSpielfeld() {
-    //Setzt den startwert der koerperform in das spielfeld
-    this.getModel.setSpielfeld(getPos.getX, getPos.getY, getID);
-
-    //Setzt die offsets der koerperform in das spielfeld
-    getForm.forEach((koerperOffset) {
-      getModel.setSpielfeld(getPos.getX + koerperOffset.getX,
-                            getPos.getY + koerperOffset.getY, getID);
-    });
-
-  }*/
 }
